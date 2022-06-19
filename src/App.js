99b2170c7641ba812eafa9f24baf4a98fa8e2c84
Bongjo Ki <bongjo@starshell.co>
2022-06-19 21:43:10 +0900

    스타일드 컴포넌트 prop 및 상속법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
 
-const Father = styled.div``;
+const Flexer = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.backgroundColor};
+  width: 100px;
+  height: 100px;
+`;
+
+// 컴포넌트끼리 스타일 상속
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
 
 function App() {
-  return <Father />;
+  return (
+    <Flexer>
+      <Box backgroundColor="teal" />
+      <Circle backgroundColor="tomato" />
+    </Flexer>
+  );
 }
 
 export default App;
